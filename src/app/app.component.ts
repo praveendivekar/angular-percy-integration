1a9cad37b3923a160ff727dcaf36ef4aaadd1a93
praveen divekar <pdivekar>
2019-09-24 17:29:08 +0530

    Final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-percy-integration';
+
+  @Output() buttonClick = new EventEmitter();
+
 }
